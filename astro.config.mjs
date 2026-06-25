59411a7282f40f2038da506bf61a923b8b15a9b2
John Choi <john.choi@reddit.com>
2026-06-25 12:19:55 -0400

    deployment configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://yohanchoi.com",
   output: "static",
   integrations: [
     tailwind({
